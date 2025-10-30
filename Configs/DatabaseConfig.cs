bbc1a1c20c4c76d24a6f0ddb767d3fdf57fd9a71
Aluminat2402 <huynhnhatthanh2402@gmail.com>
2025-10-30 15:36:23 +0700

    first_commit

---
+++ Configs/DatabaseConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyDaiLy.Configs
+{
+    internal class DatabaseConfig
+    {
+    }
+}
